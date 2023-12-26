853c97c6479a80ffcff120e6fefd1d9730651cfa
ClarkChan1 <>
2023-12-26 09:57:39 -0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
+
+function Main() {
+  return <div>hi</div>;
+}
